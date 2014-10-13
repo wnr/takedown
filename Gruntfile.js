1af8acb4440a3afea0540696c179891cf58d9e6c
Lucas Wiener <lucas@wiener.se>
2014-10-13 16:16:09 +0200

    Did some stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,15 +6,27 @@ module.exports = function(grunt) {
 	grunt.initConfig({
         connect: {
             options: {
-                port: 8000,
+                port: 8888,
                 hostname: "*",
-                keepalive: true
+                livereload: true
             },
             serve: {
-                
+                options: {
+                    open: true,
+                    base: [".", "bower_components"]
+                }
             }
+        },
+        watch: {
+            livereload: {
+                 options: {
+                     livereload: "<%= connect.options.livereload %>"
+                 },
+                 files: ["index.html", "js/**/*.js", "css/**/*.css"]               
+            }
+ 
         }
     });
 
-    grunt.registerTask("serve", ["connect:serve"]);
-}
+    grunt.registerTask("serve", ["connect:serve", "watch"]);
+};
